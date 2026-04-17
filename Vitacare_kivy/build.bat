b0c8cc17388fb033bcdc3bca5e094e7050b92a81
sfisomadonsela274-spec <sfisomadonsela274@gmail.com>
2026-04-17 15:04:26 +0200

    Delete Vitacare_kivy directory

--- Vitacare_kivy/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing PyInstaller...
-python -m pip install pyinstaller
-echo.
-echo Building VitaCare Application...
-python -m PyInstaller main.py --name VitaCare --onefile --windowed --icon=assets/icon.ico --add-data "kv;kv" --add-data "assets;assets"
-echo.
-echo Build complete! Check the 'dist' folder.
-pause
